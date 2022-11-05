dd7290e3786715cacb041150b718212c9b56a578
josar <josarroliga0943@outlook.es>
2022-11-04 23:00:26 -0600

    Primer commit master

---
+++ entidades/tbl_usuario.php
@@ -0,0 +1,18 @@
+<?php
+class tbl_usuario
+{
+    private $id_usuario;
+    private $nombres;
+    private $apellidos;
+    private $email;
+    private $estado;
+
+    public function __GET($k)
+    {
+        return $this->$k;
+    }
+    public function __SET($k, $v)
+    {
+        return $this->$k = $v;
+    }
+}
